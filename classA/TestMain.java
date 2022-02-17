a1e8c245c2413847b63f94ad4e669d620c1c47bd
kimsungun <enoch0403@gmail.com>
2022-02-17 10:33:51 +0900

    Main Test 완료

---
+++ classA/TestMain.java
@@ -0,0 +1,16 @@
+package koreait.day1; //koreait.day1은 패키지 이름
+
+public class PrintTest { //PrintTest는 클래스 이름. 시작은 반드시 대문자로
+
+	public static void main(String[] args) { 
+		// day1 첫번째 연습 : 콘솔에 출력하기
+		
+		System.out.println("Hi! Java~~ 안녕하니 자바야!");
+		//syso 입력 후 Ctrl + 스페이스바로 자동완성 하세요.
+		System.out.println("------------------------");
+		System.out.println("End!");
+		System.out.println();
+
+	}
+
+}
